8d2f539f3b8b3694cc16f87b46aaa63c34cdfc85
VasileiosAidonis <vaidonis@dundee.ac.uk>
2020-01-18 00:51:03 +0000

    damn

---
+++ images/php/app/storage/framework/views/3bb5797a04a271671e6794fbf2d5ba7e4a7dfaf7.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Summit</title>
+    <link rel="stylesheet" href="dist/app.css" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+</head>
+<body>
+    <h1>My App</h1>
+    <div id="app">
+        <h1>Hello App!</h1>
+
+
+        <router-view></router-view>
+    </div>
+    <script src="dist/app.js"></script>
+</body>
+</html>
